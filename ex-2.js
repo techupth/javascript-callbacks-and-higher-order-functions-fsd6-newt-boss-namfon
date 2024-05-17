13166b9ff1be38ba54bf07dc5ff1ebf5eff8f5c0
Namfon <varisa.vb@hotmail.com>
2024-05-17 11:24:58 +0700

    feat(ex-2): At Least Five Function

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,20 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  if (operation(array) >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
+}
+function countFive(array) {
+  let count = 0;
+  for (let value of array) {
+    if (value > 70) {
+      count++;
+    }
+  }
+  return count;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +24,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result =
+  "นักเรียนห้องที่ 1 " + atLeastFive(studentScoresRoom1, countFive);
+let scoreRoom2Result =
+  "นักเรียนห้องที่ 2 " + atLeastFive(studentScoresRoom2, countFive);
+let scoreRoom3Result =
+  "นักเรียนห้องที่ 3 " + atLeastFive(studentScoresRoom3, countFive);
+console.log(scoreRoom1Result, scoreRoom2Result, scoreRoom3Result);
