7a150e3c9eaba4d0dc8e99706eb6bcfa04f7ddbf
Namfon <varisa.vb@hotmail.com>
2024-05-17 11:09:33 +0700

    feat(ex-1): For Each Function

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,14 @@
 
 function forEach(array, operation) {
   // Start coding here
+  return operation(array);
 }
-
+function add5000(eachValue) {
+  return (eachValue += 5000);
+}
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
+newEmployeeSalaries.push(forEach(employeeSalaries, add5000));
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
