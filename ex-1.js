ef29dc02888a3a98aff22495040d9c8c9f8abcca
paphonwat <paphonwat.w@gmail.com>
2024-05-17 12:16:31 +0700

    feat:update ex1-2

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,17 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    array[i] = operation(array[i])
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+const newEmployeeSalaries = [...employeeSalaries];
 
 // Using `forEach` function here
+forEach(newEmployeeSalaries, function(salary) {
+  return salary + 5000;
+})
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,17 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let passed = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= 70) {
+      console.log("ผ่านเกณฑ์ ✅")
+      passed++
+    } else {
+      "ไม่ผ่านเกณฑ์ ❌"
+    }
+    array[i] = operation(array[i])
+  }
+  return passed >=5;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +20,16 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, function(result){
+  return result;
+})
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, function(result){
+  return result;
+})
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, function(result){
+  return result;
+})
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result ? 'ผ่านเกณฑ์ ✅' : 'ไม่ผ่านเกณฑ์ ❌'}`)
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result ? 'ผ่านเกณฑ์ ✅' : 'ไม่ผ่านเกณฑ์ ❌'}`)
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result ? 'ผ่านเกณฑ์ ✅' : 'ไม่ผ่านเกณฑ์ ❌'}`)
